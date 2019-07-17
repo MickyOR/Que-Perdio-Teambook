1193f4cb4cf95140027b45852e7f692f608023b3
Rodrigo Salguero <33531552+pacha2880@users.noreply.github.com>
2019-07-16 22:03:51 -0300

    nuevo

--- Geometria/ConvexHull(Graham's Scan).cpp
+++ Geometria/ConvexHull(Graham's Scan).cpp
@@ -1,76 +1,68 @@
-
 struct point
 {
-	double x,y;
-	point() {x=y=0;}
-	point(double X, double Y): x(X), y(Y) {}
+	double x, y;
+	point() {}
+	point(double x, double y) : x(x), y(y){}
 };
 double dist(point a, point b)
 {
-	return hypot(a.x-b.x,a.y-b.y);
+	return hypot(a.x - b.x, a.y - b.y);
 }
-point tovec(point a, point b)
-{
-	return point(b.x-a.x, b.y-a.y);
-}
-double cross(point a, point b)
+double cross2(point a, point b)
 {
 	return a.x*b.y - a.y*b.x;
 }
-bool coolinear(point a, point b, point c)
+point tovec(point a, point b)
 {
-	return fabs(cross(tovec(a,b), tovec(a,c))) < EPS;
+	return point(b.x - a.x, b.y - a.y);
 }
-bool ccw(point a, point b, point c)
+double cross(point a, point b, point c)
 {
-	return cross(tovec(a,b),tovec(a,c)) >= 0;
+	return cross2(tovec(a, b), tovec(a, c));
 }
-point pivot;
-bool anComp(point a, point b)
+bool eq(double a, double b)
 {
-	if(coolinear(pivot, a, b))
-		return dist(pivot, a) < dist(pivot, b);
-	a = tovec(pivot, a), b = tovec(pivot, b);
-	return atan2(a.y,a.x) < atan2(b.y,b.x);
+	return fabs(a-b) < EPS;
 }
-vector<point> ch(vector<point> p)
+point mini;
+bool comp(point a, point b)
 {
-	int n = p.size(), i , j;
-	if(n<=3) 
+	point ta = tovec(mini, a);
+	point tb = tovec(mini, b);
+	double ana = atan2(ta.y, ta.x), anb = atan2(tb.y, tb.x);
+	if(eq(ana, anb))
+		return dist(a, mini) < dist(b, mini);
+	return ana < anb - EPS;
+}
+//no hay 3 puntos colineales
+vector<point> hull(vector<point> p)
+{
+	for(int i = 1; i < p.size(); i++)
 	{
-		if(p[0].x!=p[n-1].x||p[0].y!=p[n-1].y) p.pb(p[0]);
-		return p;
-	}
-	int in=0;
-	for(int i = 0; i< n; i++)
-		if(p[i].y < p[in].y || p[i].x < p[in].x && p[i].y == p[in].y)
-			in = i;
-	pivot = p[in];
-	for(int i = 0; i<n;i++)
-	{
-		cout<<p[i].x<<' '<<p[i].y<<endl;
-	}
-	cout<<endl;
-	swap(p[0],p[in]);
-	cout<<pivot.x<<'#'<<pivot.y<<endl;
-	sort(++p.begin(), p.end(),anComp);
-	for(int i = 0; i<n;i++)
-	{
-		cout<<p[i].x<<' '<<p[i].y<<endl;
-	}
-	cout<<endl;
-	i = 2;
-	vector<point> s = {p[n-1], p[0], p[1]};
-	while(i < n && s.size()>0)
-	{
-		j = s.size();
-		if(ccw(s[j-2],s[j-1],p[i]) || j == 2)
+		if(eq(p[i].y, p[0].y) )
 		{
-			s.pb(p[i]);
-			i++;
+			if(p[i].x < p[0].x - EPS)
+				swap(p[i], p[0]);
 		}
 		else
-			s.pop_back();
+		{
+			if(p[i].y < p[0].y - EPS)
+				swap(p[i], p[0]);
+		}
 	}
-	return s;
+	mini = p[0];
+	sort(++p.begin(), p.end(), comp);
+	p.pb(p[0]);
+	vector<point> res;
+	res.pb(p[0]);
+	res.pb(p[1]);
+	for(int i = 2; i < p.size(); i++)
+	{
+		while(cross(res[res.size()-2], res.back(), p[i]) < EPS)
+		{	
+			res.pop_back();
+		}
+		res.pb(p[i]);
+	}
+	return res;
 }
