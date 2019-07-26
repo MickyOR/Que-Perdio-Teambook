293b3ed5c1deb0a2d92c714617b0f6c1a4d8a108
Rodrigo Salguero <33531552+pacha2880@users.noreply.github.com>
2019-07-26 11:48:09 -0400

    Update KMP.cpp

--- Strings/KMP.cpp
+++ Strings/KMP.cpp
@@ -1,15 +1,15 @@
 vector<int> kmp(string s)
 {
-  int n = s.size();
-  vector<int> pi(n, 0);
-  for(int i = 1; i < n; i++)
-  {
-    int j = pi[i-1];
-    while(j > 0 && s[j] != s[i])
-      j = pi[j-1];
-    if(s[j] == s[i])
-      j++;
-    pi[i] = j;
-  }
-  return pi;
+	int n = s.size();
+	vector<int> pi(n, 0);
+	for(int i = 1; i < n; i++)
+	{
+    	int j = pi[i-1];
+    	while(j > 0 && s[j] != s[i])
+      		j = pi[j-1];
+    	if(s[j] == s[i])
+      		j++;
+    	pi[i] = j;
+  	}
+  	return pi;
 }
