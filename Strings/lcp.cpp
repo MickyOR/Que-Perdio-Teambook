94c492a9cd5364595f3d49e5f1f91a50dec46754
Rodrigo Salguero <33531552+pacha2880@users.noreply.github.com>
2019-08-02 13:33:36 -0400

    Update lcp.cpp

--- Strings/lcp.cpp
+++ Strings/lcp.cpp
@@ -2,18 +2,15 @@ vector<int> lcp(string &s, vector<int> &sa)
 {
 	int n = s.size(), k, z = 0;
 	vector<int> sa1(n), lcp(n);
-	fore(i, 0, n)
-		sa1[sa[i]] = i;
+	fore(i, 0, n) sa1[sa[i]] = i;
 	fore(i, 0, n)
 	{
 		k = sa1[i];
-		if(k < n-1)
-		{
-			lcp[k] = z;
-			while(s[sa[k] + lcp[k]] == s[sa[k+1] + lcp[k]])
-				lcp[k]++;
-		}
-		z = max(0, lcp[k] - 1);
+		if(k < n)
+			while(s[i + z] == s[sa[k+1] + z])
+				z++;
+		lcp[k] = z;
+		z = max(z-1, 0);
 	}
 	return lcp;
 }
