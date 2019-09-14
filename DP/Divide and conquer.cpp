4fbbd24508de180833f88dbd6ecd007a18f536d0
Miguel Angel Ortiz Merida <mickyor97@gmail.com>
2019-09-14 11:39:06 -0400

    Create Divide and conquer.cpp

---
+++ DP/Divide and conquer.cpp
@@ -0,0 +1,21 @@
+ll lastDP[tam], DP[tam];
+int C[tam][tam];
+
+void DC(int b, int e, int KL, int KR)
+{
+	int mid = (b + e) / 2;
+	pair<ll, int> best = mp(-1, KL);
+
+	for (int k = KL; k < min(mid, KR+1); k++)
+	{
+		best = max( best, mp(lastDP[k] + C[k+1][mid], k) );
+	}
+
+	DP[mid] = best.first;
+	int K = best.second;
+
+	if (b <= mid-1)
+		DC(b, mid-1, KL, K);
+	if (mid+1 <= e)
+		DC(mid+1, e, K, KR);
+}
