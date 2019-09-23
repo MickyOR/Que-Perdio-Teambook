8672d66d330595ef813978b8c4ae97b52026a968
Rodrigo Salguero <33531552+pacha2880@users.noreply.github.com>
2019-09-23 00:23:36 -0400

    Update Queue_with_min.cpp

--- Estructuras de Datos/Queue_with_min.cpp
+++ Estructuras de Datos/Queue_with_min.cpp
@@ -33,3 +33,45 @@ struct quemin
 		return mini;
 	}
 };
+
+struct quemin
+{
+	pair<int,int> bo[100010], to[100010];
+	int boto = -1, toto = -1, ax;
+	void push(int n)
+	{
+		ax = boto + 1;
+		if(boto == -1)
+			bo[ax] = mp(n, n);	
+		else
+			bo[ax] = mp(n, min(bo[boto].s, n));
+		boto++;
+	}
+	void pop()
+	{
+		if(toto == -1)
+		{
+			while(boto > -1)
+			{
+				ax = toto + 1;
+				if(toto == -1)
+					to[ax] = mp(bo[boto].f, bo[boto].f);
+				else
+					to[ax] = mp(bo[boto].f, min(bo[boto].f, to[toto].s));
+				toto++;
+				boto--;
+			}
+		}
+		if(toto > -1)
+			toto--;
+	}
+	int mini()
+	{
+		int mini = MOD;
+		if(boto > -1)
+			mini = bo[boto].s;
+		if(toto > -1)
+			mini = min(mini, to[toto].s);
+		return mini;
+	}
+};
