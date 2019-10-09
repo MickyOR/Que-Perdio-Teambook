5ba811da7b5eb9d80724fcbcae80098515047c64
Rodrigo Salguero <33531552+pacha2880@users.noreply.github.com>
2019-10-09 18:33:54 -0400

    Create Lattice points.cpp

---
+++ Geometria/Lattice points.cpp
@@ -0,0 +1,21 @@
+//calculates number of integer points (x ; y) such for 0≤x<n and 0 < y ≤ ⌊kx+b⌋
+int count_lattices(double k, double b, long long n) {
+    auto fk = floor(k);
+    auto fb = floor(b);
+    auto cnt = 0LL;
+    if (k > 1 - EPS || b > 1 -EPS) {
+        cnt += (fk * (n - 1) + 2 * fb) * n / 2;
+        k -= fk;
+        b -= fb;
+    }
+    auto t = k * n + b;
+    auto ft = floor(t);
+    if (ft > 1 - EPS) {
+        cnt += count_lattices(1 / k, (t - floor(t)) / k, floor(t));
+    }
+    return cnt;
+}
+pair<double, double> kb(point a, point b)
+{
+	return mp((b.y - a.y) / (b.x - a.x), (a.y * b.x - a.x * b.y) / (b.x - a.x));
+}
