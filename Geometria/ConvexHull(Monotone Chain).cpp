4b833e2b48bb4c70572813a55c0ccb395a64df86
Rodrigo Salguero <33531552+pacha2880@users.noreply.github.com>
2019-10-27 14:52:30 -0400

    Update ConvexHull(Monotone Chain).cpp

--- Geometria/ConvexHull(Monotone Chain).cpp
+++ Geometria/ConvexHull(Monotone Chain).cpp
@@ -1,19 +1,19 @@
 
 vector<point> hull(vector<point> p)
 {
-	vector<point> h;
-	sort(p.begin(), p.end());
 	int n = p.size();
+	vector<point> h;
+	sort(all(p));
 	fore(i, 0, n)
 	{
-		while(h.size() >= 2 && ((h.back() - h[h.size()-2]) ^ (p[i] - h[h.size()-2])) >= 0) h.pop_back();
-		h.pb(p[i]);
+		while(h.size() >= 2 && p[i].left(h[sz(h) - 2], h.back())) h.pop_back();
+		h.push_back(p[i]);
 	}
 	h.pop_back();
 	int k = h.size();
 	for(int i = n-1; i > -1; i--)
 	{
-		while(h.size() >= k + 2 && ((h.back() - h[h.size()-2]) ^ (p[i] - h[h.size()-2])) >= 0) h.pop_back();
+		while(h.size() >= k + 2 && p[i].left(h[sz(h) - 2], h.back())) h.pop_back();
 		h.pb(p[i]);
 	}
 	h.pop_back();
