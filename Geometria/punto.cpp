33452e1abb78df56af8a4c8651f5ddb0873d4199
Rodrigo Salguero <33531552+pacha2880@users.noreply.github.com>
2019-10-27 14:40:49 -0400

    Create punto.cpp

---
+++ Geometria/punto.cpp
@@ -0,0 +1,15 @@
+//typedef long long ll;
+//typedef long double ll;
+
+struct point
+{
+	ll x, y;
+	point() {}
+	point(ll x, ll y): x(x), y(y) {}
+	point operator -(point p) {return point(x - p.x, y - p.y);}
+	ll operator ^(point p) {return x * p.y - y * p.x;}
+  ll operator *(point p) {return x * p.x + y * p.y;}
+  point operator *(ll a) {return point(x * a, y * a);}
+	bool operator <(point p) {return x == p.x ? y < p.y : x < p.x;}
+	bool left(point a, point b) {return ((b - a) ^ (*this - a)) >= 0;}
+};
