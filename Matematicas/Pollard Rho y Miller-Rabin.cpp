2bbfbc34f99f3fe910f74b822ed725e8f5d4c94e
Rodrigo Salguero <33531552+pacha2880@users.noreply.github.com>
2019-11-06 23:44:37 -0400

    Update Pollard Rho y Miller-Rabin.cpp

--- Matematicas/Pollard Rho y Miller-Rabin.cpp
+++ Matematicas/Pollard Rho y Miller-Rabin.cpp
@@ -1,59 +1,87 @@
-ll mulmod(ll a, ll b, ll c) //multiplicacion modular binaria
-{
-	ll x =0, y = a % c;
-	while(b > 0)
-	{
-		if(b % 2 == 1) x = (x + y) % c;
-		y = (y * 2) % c;
-		b /= 2;
-	}
-	return x % c;
-}
-ll gcd(ll a, ll b) {return (b==0? a : gcd(b,a%b));}
-ll poll(ll n) //numoro grande p = a*b devuelve a  (a y b primos)
-{
-	srand (time(NULL));
-	ll x = rand()%(n-2) + 2, y = x, c = rand()%(n-1) + 1, d = 1;
-	while(d==1)
-	{
-		x = (mulmod(x, x, n) + c) % n;
-		y = (mulmod(y, y, n) + c) % n;
-		y = (mulmod(y, y, n) + c) % n;
-		d = gcd((x>y?x-y:y-x),n);
-		if(d == n) return poll(n);
-	}
-	return d;
-}
-ll modpow(ll b, ll e, ll m)
-{
-	if(e == 0) return 1;
-	//return (modpow((b*b) % m, e/2, m) * (e & 1? b : 1)) % m;
-	return mulmod(modpow(mulmod(b,b,m), e/2, m), (e & 1? b : 1), m);
-}
-bool mill(ll n, int k) //k = 10 es seguro 
-{
-	ll p = n - 1;
-	int r = 0;
-	bool pp;
-	while(p % 2 == 0)
-	{
-		p /= 2;
-		r++;
-	}
-	for(int i =0; i < k; i++)
-	{
-		ll a = rand()%(n-4) + 2;
-		ll exp = modpow(a,p,n);
-		if(exp == 1 || exp == n-1) return true;
-		pp = false;
-		for(int j = 0; j < r; j++)
-		{
-			exp = mulmod(exp, exp, n);
-			if(exp == 1) return false;
-			if(exp == n-1) {pp = true; break;}
-		}
-		if(!pp) return false;
-	}
+//copiado del vasito
 
+ll gcd(ll a, ll b){return a?gcd(b%a,a):b;}
+ll mulmod(ll a, ll b, ll m) {
+	ll r=a*b-(ll)((long double)a*b/m+.5)*m;
+	return r<0?r+m:r;
+}
+ll expmod(ll b, ll e, ll m){
+	if(!e)return 1;
+	ll q=expmod(b,e/2,m);q=mulmod(q,q,m);
+	return e&1?mulmod(b,q,m):q;
+}
+bool is_prime_prob(ll n, int a){
+	if(n==a)return true;
+	ll s=0,d=n-1;
+	while(d%2==0)s++,d/=2;
+	ll x=expmod(a,d,n);
+	if((x==1)||(x+1==n))return true;
+	fore(_,0,s-1){
+		x=mulmod(x,x,n);
+		if(x==1)return false;
+		if(x+1==n)return true;
+	}
+	return false;
+}
+bool rabin(ll n){ // true iff n is prime
+	if(n==1)return false;
+	int ar[]={2,3,5,7,11,13,17,19,23};
+	fore(i,0,9)if(!is_prime_prob(n,ar[i]))return false;
 	return true;
+}
+ll rho(ll n){
+	if(!(n&1))return 2;
+	ll x=2,y=2,d=1;
+	ll c=rand()%n+1;
+	while(d==1){
+		x=(mulmod(x,x,n)+c)%n;
+		y=(mulmod(y,y,n)+c)%n;
+		y=(mulmod(y,y,n)+c)%n;
+		if(x>=y)d=gcd(x-y,n);
+		else d=gcd(y-x,n);
+	}
+	return d==n?rho(n):d;
+}
+void fact(ll n, map<ll,int>& f){ //O (lg n)^3
+	if(n==1)return;
+	if(rabin(n)){f[n]++;return;}
+	ll q=rho(n);fact(q,f);fact(n/q,f);
+}
+// optimized version: replace rho and fact with the following:
+const int MAXP=1e6+1; // sieve size
+int sv[MAXP]; // sieve
+ll add(ll a, ll b, ll m){return (a+=b)<m?a:a-m;}
+ll rho(ll n){
+	static ll s[MAXP];
+	while(1){
+		ll x=rand()%n,y=x,c=rand()%n;
+		ll *px=s,*py=s,v=0,p=1;
+		while(1){
+			*py++=y=add(mulmod(y,y,n),c,n);
+			*py++=y=add(mulmod(y,y,n),c,n);
+			if((x=*px++)==y)break;
+			ll t=p;
+			p=mulmod(p,abs(y-x),n);
+			if(!p)return gcd(t,n);
+			if(++v==26){
+				if((p=gcd(p,n))>1&&p<n)return p;
+				v=0;
+			}
+		}
+		if(v&&(p=gcd(p,n))>1&&p<n)return p;
+	}
+}
+void init_sv(){
+	fore(i,2,MAXP)if(!sv[i])for(ll j=i;j<MAXP;j+=i)sv[j]=i;
+}
+void fact(ll n, map<ll,int>& f){ // call init_sv first!!!
+	for(auto&& p:f){
+		while(n%p.fst==0){
+			p.snd++;
+			n/=p.fst;
+		}
+	}
+	if(n<MAXP)while(n>1)f[sv[n]]++,n/=sv[n];
+	else if(rabin(n))f[n]++;
+	else {ll q=rho(n);fact(q,f);fact(n/q,f);}
 }
