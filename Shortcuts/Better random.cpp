33b32091e7371a2287971663045eb86f3709a136
Miguel Angel Ortiz Merida <mickyor97@gmail.com>
2020-02-07 11:49:21 -0400

    Create Better random.cpp

---
+++ Shortcuts/Better random.cpp
@@ -0,0 +1,8 @@
+t19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+// mt19937_64 para 64 bits
+cout << rng() << endl;
+
+vi permutation(10);
+fore(i, 0, 10) permutation[i] = i;
+	shuffle(permutation.begin(), permutation.end(), rng);
+fore(i, 0, 10) cout << permutation[i] << ' ';
