0dc53866726d312e6fc2efac425e8b2d0b978764
Rodrigo Salguero <33531552+pacha2880@users.noreply.github.com>
2020-08-31 17:20:32 -0400

    Update Point in Poly (convex).cpp

--- Geometria/Point in Poly (convex).cpp
+++ Geometria/Point in Poly (convex).cpp
@@ -2,24 +2,22 @@
 bool ins(vector<point> &pol, point p)
 {
 	fore(i, 0, pol.size()-1)
-	{
-		if(((pol[i+1] - pol[i]) ^ (p - pol[i])) > 0)
+		if(!p.left(pol[i], pol[i + 1])
 			return false;
-	}
 	return true;
 }
-// logaritmico clockwise
+// logaritmico counterclockwise
 bool inslog(vector<point> &pol, point p)
 {
 	int lo = 1, hi = pol.size() - 2, mid;
-	if(p.left(pol[0], pol[1]) || p.left(pol[hi], pol[0])) return false;
+	if(p.left(pol[1], pol[0]) || p.left(pol[0], pol[hi])) return false;
 	while(lo < hi - 1)
 	{
 		mid = (lo + hi) / 2;
 		if(p.left(pol[0], pol[mid]))
-			hi = mid;
-		else
 			lo = mid;
+		else
+			hi = mid;
 	}
-	return p.left(pol[hi], pol[lo]);
+	return p.left(pol[lo], pol[hi]);
 }
