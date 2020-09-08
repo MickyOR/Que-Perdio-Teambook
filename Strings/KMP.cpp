ea46b4cdfb265b779c85920b1df48f29b031bd02
Miguel Angel Ortiz Merida <mickyor97@gmail.com>
2020-09-08 17:33:49 -0400

    añadido kmp automata

--- Strings/KMP.cpp
+++ Strings/KMP.cpp
@@ -13,3 +13,23 @@ vector<int> kmp(string s)
   	}
   	return pi;
 }
+
+const int tam = 1e5 + 10;
+int aut[tam][26];
+
+void genAut(string &s) {
+    clr(aut, 0);
+    vi pi = kmp(s);
+    int n = s.size();
+    fore(i, 0, n) {
+        aut[i][s[i]-'a'] = i+1;
+    }
+    fore(i, 0, n+1) {
+        for (int c = 0; c < 26; c++) {
+            if (aut[i][c]) continue;
+            if (i < n && s[i] == 'a' + c) aut[i][c] = i+1;
+            else if (i > 0) aut[i][c] = aut[pi[i-1]][c];
+            else aut[i][c] = 0;
+        }
+    }
+}
