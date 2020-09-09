f9b4d0577f6cca80ea8125629f4511a0efc15988
Rodrigo Salguero <33531552+pacha2880@users.noreply.github.com>
2020-09-08 20:36:50 -0400

    Create Strirling y Bell.cpp

---
+++ Matematicas/Strirling y Bell.cpp
@@ -0,0 +1,19 @@
+
+ll stir[tam][tam];
+ll bell[tam];
+void stirBell()
+{
+  fore(i, 1, tam)
+  {
+    stir[i][1] = 1;
+    fore(j, 2, 1010)
+    {
+      stir[i][j] = (j * stir[i - 1][j] % MOD + stir[i - 1][j - 1]) % MOD;
+    }
+  }
+  fore(i, 1, tam)
+  {
+    fore(j, 1, i + 1)
+      bell[i] = (bell[i] + stir[i][j]) % MOD;
+  }
+}
