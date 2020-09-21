785c5630100a2076e497d9a489f4372a1892c900
Miguel Angel Ortiz Merida <mickyor97@gmail.com>
2020-09-21 15:40:58 -0400

    Codigo mas general

--- Grafos/dinic_muchos_nodos.cpp
+++ Grafos/dinic_muchos_nodos.cpp
@@ -1,80 +1,65 @@
-const int tam = 20010;
-
-int S, T;
-int n, m;
-int dist[tam], q[tam], work[tam];
-
-struct Edge {
-	int to, rev;
-	int f, cap;
+struct flowEdge
+{
+    int to, rev, f, cap;
 };
 
-vector<Edge> G[tam];
+vector<vector<flowEdge> > G;
 
-void addEdge(int s, int t, int cap)
-{
-	Edge a = {t, (int)G[t].size(), 0, cap};
-	Edge b = {s, (int)G[s].size(), 0, 0};
-	G[s].pb(a);
-	G[t].pb(b);
+// Añade arista (st -> en) con su capacidad
+void addEdge(int st, int en, int cap) {
+    flowEdge A = {en, (int)G[en].size(), 0, cap};
+    flowEdge B = {st, (int)G[st].size(), 0, 0};
+    G[st].pb(A);
+    G[en].pb(B);
 }
 
-bool dinic_BFS(){
-	fill(dist, dist + n + 2, -1);
-	dist[S] = 0;
-	int qt = 0;
-	q[qt++] = S;
-	for (int qh = 0; qh < qt; qh++)
-	{
-		int u = q[qh];
-		for (int j = 0; j < G[u].size(); j++)
-		{
-			Edge &e = G[u][j];
-			int v = e.to;
-			if (dist[v] < 0 && e.f < e.cap)
-			{
-				dist[v] = dist[u] + 1;
-				q[qt++] = v;
-			}
-		}
-	}
-	return dist[T] >= 0;
+int nodes, S, T; // asignar estos valores al armar el grafo G
+                 // nodes = nodos en red de flujo. Hacer G.clear(); G.resize(nodes);
+vi work, lvl;
+int Q[200010];
+
+bool bfs() {
+    int qt = 0;
+    Q[qt++] = S;
+    lvl.assign(nodes, -1);
+    lvl[S] = 0;
+    for (int qh = 0; qh < qt; qh++) {
+        int v = Q[qh];
+        for (flowEdge &e : G[v]) {
+            int u = e.to;
+            if (e.cap <= e.f || lvl[u] != -1) continue;
+            lvl[u] = lvl[v] + 1;
+            Q[qt++] = u;
+        }
+    }
+    return lvl[T] != -1;
 }
 
-int dinic_DFS(int u, int f)
-{
-	if (u == T || f == 0) return f;
-	for (int &i = work[u]; i < G[u].size(); i++)
-	{
-		Edge &e = G[u][i];
-		if (e.cap <= e.f) continue;
-		int v = e.to;
-		if (dist[v] == dist[u] + 1)
-		{
-			int df = dinic_DFS(v, min(f, e.cap - e.f));
-			if (df > 0)
-			{
-				e.f += df;
-				G[v][e.rev].f -= df;
-				return df;
-			}
-		}
-	}
-	return 0;
+int dfs(int v, int f) {
+    if (v == T || f == 0) return f;
+    for (int &i = work[v]; i < G[v].size(); i++) {
+        flowEdge &e = G[v][i];
+        int u = e.to;
+        if (e.cap <= e.f || lvl[u] != lvl[v] + 1) continue;
+        int df = dfs(u, min(f, e.cap - e.f));
+        if (df) {
+            e.f += df;
+            G[u][e.rev].f -= df;
+            return df;
+        }
+    }
+    return 0;
 }
 
-int maxFlow()
-{
-	int flow = 0;
-	while ( dinic_BFS() )
-	{
-		fill(work, work + n + 2, 0);
-		while (true)
-		{
-			int df = dinic_DFS(S, INF);
-			if (df == 0) break;
-			flow += df;
-		}
-	}
-	return flow;
+int maxFlow() {
+    int flow = 0;
+    while (bfs()) {
+        work.assign(nodes, 0);
+        while (true) {
+            int df = dfs(S, INF);
+            if (df == 0) break;
+            flow += df;
+        }
+    }
+    return flow;
 }
