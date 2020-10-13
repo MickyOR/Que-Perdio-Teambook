a66e88c183f52ab163d47a495a7d2cab6c4c2f2f
Rodrigo Salguero <33531552+pacha2880@users.noreply.github.com>
2020-10-13 16:24:28 -0400

    Update Euclid.cpp

--- Matematicas/Euclid.cpp
+++ Matematicas/Euclid.cpp
@@ -2,7 +2,7 @@
 ll gcd(ll a, ll b){return b==0? a:gcd(b,a%b);}
 
 int x, y, d;
-void extendedEuclid(int a, int b)//ecuacion diofantica ax + by = 1
+void extendedEuclid(int a, int b)//ecuacion diofantica ax + by = d
 {
 	if(b==0) {x=1; y=0; d=a; return;}
 	extendedEuclid(b,a%b);
