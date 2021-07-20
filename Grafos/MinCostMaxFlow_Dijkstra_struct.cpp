85ec58dc607f9d5c80c704e9555529b5954a38ed
Miguel Angel Ortiz Merida <mickyor97@gmail.com>
2021-07-19 22:45:55 -0400

    MCMF con struct
    Modificado de kactl para nuestro template

---
+++ Grafos/MinCostMaxFlow_Dijkstra_struct.cpp
@@ -0,0 +1,85 @@
+// https://github.com/kth-competitive-programming/kactl/blob/main/content/graph/MinCostMaxFlow.h
+
+// #include <bits/extc++.h> // <- Incluir
+
+const ll INF = numeric_limits<ll>::max() / 4;
+
+// Aprox: O(E^2)
+// Recordar que tambien esta acotado por el flujo maximo
+
+struct MCMF {
+	int N;
+	vector<vi> ed, red;
+	vector<vll> cap, flow, cost;
+	vi seen;
+	vll dist, pi;
+	vector<ii> par;
+
+	MCMF(int N) :
+		N(N), ed(N), red(N), cap(N, vll(N)), flow(cap), cost(cap),
+		seen(N), dist(N), pi(N), par(N) {}
+
+	void addEdge(int from, int to, ll cap, ll cost) {
+		this->cap[from][to] = cap;
+		this->cost[from][to] = cost;
+		ed[from].push_back(to);
+		red[to].push_back(from);
+	}
+
+	void path(int s) {
+		fill(all(seen), 0);
+		fill(all(dist), INF);
+		dist[s] = 0; ll di;
+
+		__gnu_pbds::priority_queue<pair<ll, int>> q;
+		vector<decltype(q)::point_iterator> its(N);
+		q.push({0, s});
+
+		auto relax = [&](int i, ll cap, ll cost, int dir) {
+			ll val = di - pi[i] + cost;
+			if (cap && val < dist[i]) {
+				dist[i] = val;
+				par[i] = {s, dir};
+				if (its[i] == q.end()) its[i] = q.push({-dist[i], i});
+				else q.modify(its[i], {-dist[i], i});
+			}
+		};
+
+		while (!q.empty()) {
+			s = q.top().second; q.pop();
+			seen[s] = 1; di = dist[s] + pi[s];
+			for (int i : ed[s]) if (!seen[i])
+				relax(i, cap[s][i] - flow[s][i], cost[s][i], 1);
+			for (int i : red[s]) if (!seen[i])
+				relax(i, flow[i][s], -cost[i][s], 0);
+		}
+		fore(i,0,N) pi[i] = min(pi[i] + dist[i], INF);
+	}
+
+	pair<ll, ll> maxflow(int s, int t) {
+		ll totflow = 0, totcost = 0;
+		while (path(s), seen[t]) {
+			ll fl = INF;
+			for (int p,r,x = t; tie(p,r) = par[x], x != s; x = p)
+				fl = min(fl, r ? cap[p][x] - flow[p][x] : flow[x][p]);
+			totflow += fl;
+			for (int p,r,x = t; tie(p,r) = par[x], x != s; x = p)
+				if (r) flow[p][x] += fl;
+				else flow[x][p] -= fl;
+		}
+		fore(i,0,N) fore(j,0,N) totcost += cost[i][j] * flow[i][j];
+		return {totflow, totcost};
+	}
+
+	// If some costs can be negative, call this before maxflow:
+	void setpi(int s) { // (otherwise, leave this out)
+		fill(all(pi), INF); pi[s] = 0;
+		int it = N, ch = 1; ll v;
+		while (ch-- && it--)
+			fore(i,0,N) if (pi[i] != INF)
+				for (int to : ed[i]) if (cap[i][to])
+					if ((v = pi[i] + cost[i][to]) < pi[to])
+						pi[to] = v, ch = 1;
+		assert(it >= 0); // negative cost cycle
+	}
+};
