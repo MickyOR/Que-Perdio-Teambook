d11f163fa72e20534f746c9448ab12f99ac10c9d
Rodrigo Salguero <33531552+pacha2880@users.noreply.github.com>
2021-08-02 22:54:36 -0400

    Create Sparse_Table_generico.cpp

---
+++ Estructuras de Datos/Sparse_Table_generico.cpp
@@ -0,0 +1,21 @@
+template<typename it, typename bin_op>
+struct sparse_table {
+ 
+    using T = typename remove_reference<decltype(*declval<it>())>::type;
+    vector<vector<T>> t; bin_op f;
+ 
+    sparse_table(it first, it last, bin_op op) : t(1), f(op) {
+        int n = distance(first, last);
+        t.assign(32-__builtin_clz(n), vector<T>(n));
+        t[0].assign(first, last);
+        for (int i = 1; i < t.size(); i++)
+            for (int j = 0; j < n-(1<<i)+1; j++)
+                t[i][j] = f(t[i-1][j], t[i-1][j+(1<<(i-1))]);
+    }
+ 
+    // returns f(a[l..r]) in O(1) time
+    T query(int l, int r) {
+        int h = floor(log2(r-l+1));
+        return f(t[h][l], t[h][r-(1<<h)+1]);
+    }
+};
